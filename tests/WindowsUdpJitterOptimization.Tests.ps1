dc77ba4b3bcde001f95cea56cc2af89e3815664a
Sebastian Spicker <sebastian.spicker@googlemail.com>
2026-02-12 13:17:28 +0100

    fix ci

--- tests/WindowsUdpJitterOptimization.Tests.ps1
+++ tests/WindowsUdpJitterOptimization.Tests.ps1
@@ -9,14 +9,6 @@ Describe 'WindowsUdpJitterOptimization repo' {
     (Get-Command -Name Invoke-UdpJitterOptimization -ErrorAction Stop).CommandType | Should -Be 'Function'
   }
 
-  It 'keeps deprecated scripts as thin wrappers' {
-    $reset = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '../deprecated/reset-udp-jitter.ps1') -Raw
-    $opt = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath '../deprecated/optimize-udp-jitter.ps1') -Raw
-    $reset | Should -Match 'ResetDefaults'
-    $opt | Should -Match '\.\./optimize-udp-jitter\.ps1'
-    $opt | Should -Match '@args'
-  }
-
   It 'does not use Invoke-Expression' {
     $hits = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath '..') -Recurse -File -Include '*.ps1', '*.psm1' |
       Where-Object { $_.FullName -notmatch '[\\\\/]tests[\\\\/]' } |
